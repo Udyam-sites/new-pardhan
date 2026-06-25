98db150317261c9304988ba376896f193256bd5e
keshvam sharma <sharmakeshvam96@gmail.com>
2026-06-25 16:57:03 +0530

    sixp-heritage-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import HomeCarousel from './components/HeroSection'
 import FlavourAmbassadorSection from './components/historySection'
+import HeritageSection from './components/HeritageSection'
+
+import SuccessSection from './components/sixp'
+
 
 const page = () => {
   return (
     <div>
       <HomeCarousel/>
+      <HeritageSection/>
+      <SuccessSection/>
       <FlavourAmbassadorSection/>
     </div>
   )
